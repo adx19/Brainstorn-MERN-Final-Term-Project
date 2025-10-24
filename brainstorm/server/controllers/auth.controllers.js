c5349107203f3f2249498e62575dc4d77136136f
Abhijit P <pabhijit047@gmail.com>
2025-10-24 23:44:03 +0530

    Testing

--- brainstorm/server/controllers/auth.controllers.js
+++ brainstorm/server/controllers/auth.controllers.js
@@ -56,8 +56,9 @@ export const login = async (req, res) => {
     // Send JWT as httpOnly cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production", // must be HTTPS in prod
+      sameSite: "none", // allow cross-origin requests
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
     res.status(200).json({ user: existingUser });
   } catch (err) {
